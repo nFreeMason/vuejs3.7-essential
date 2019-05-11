e304853c5e4ca51acdc2de8eea515d11a2de63ef
nFreeMason <1502534146@qq.com>
2019-05-11 11:58:06 +0800

    github pages

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-  baseUrl: "/", // 部署应用时的根路径(默认'/'),也可用相对路径(存在使用限制)
+  publicPath: "/", // 部署应用时的根路径(默认'/'),也可用相对路径(存在使用限制)
   outputDir: "dist", // 运行时生成的生产环境构建文件的目录(默认''dist''，构建之前会被清除)
   assetsDir: "public", //放置生成的静态资源(s、css、img、fonts)的(相对于 outputDir 的)目录(默认'')
   indexPath: "index.html", //指定生成的 index.html 的输出路径(相对于 outputDir)也可以是一个绝对路径。
+  // Default: true默认情况下
+  // 生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存。然而
+  // 这也要求 index 的 HTML 是被 Vue CLI 自动生成的。如果你无法使用 Vue CLI 生成的 index HTML，
+  // 你可以通过将这个选项设为 false 来关闭文件名哈希
+  filenameHashing: false,
+
   pages: {
     //pages 里配置的路径和文件名在你的文档目录必须存在 否则启动服务会报错
     index: {
@@ -14,7 +20,7 @@ module.exports = {
       chunks: ["chunk-vendors", "chunk-common", "index"] // 在这个页面中包含的块，默认情况下会包含,提取出来的通用 chunk 和 vendor chunk
     }
   },
-  lintOnSave: true, // 是否在保存的时候检查
+  lintOnSave: false, // process.env.NODE_ENV !== 'production', // 是否在保存的时候检查
   productionSourceMap: true, // 生产环境是否生成 sourceMap 文件
   css: {
     extract: true, // 是否使用css分离插件 ExtractTextPlugin
@@ -23,26 +29,30 @@ module.exports = {
     modules: false // 启用 CSS modules for all css / pre-processor files.
   },
   //反向代理
-  // devServer: {
-  //   // 环境配置
-  //   host: '192.168.1.53',
-  //   port: 8080,
-  //   https: false,
-  //   hotOnly: false,
-  //   open: true, //配置自动启动浏览器
-  //   proxy: {
-  //     // 配置多个代理(配置一个 proxy: 'http://localhost:4000' )
-  //     // '/api': {
-  //     //   target: 'http://192.168.1.248:9888',
-  //     //   // target: 'http://192.168.1.4:8999',
-  //     //   pathRewrite: {
-  //     //     '^/api': '/api'
-  //     //   }
-  //     // }
-  //   }
-  // },
-  pluginOptions: {
-    // 第三方插件配置
-    // ...
-  }
+  devServer: {
+    overlay: {
+      warnings: false,
+      errors: false
+    },
+    // 环境配置
+    // host: '192.168.1.53',
+    // port: 8080,
+    // https: false,
+    // hotOnly: false,
+    // open: true, //配置自动启动浏览器
+    // proxy: {
+      // 配置多个代理(配置一个 proxy: 'http://localhost:4000' )
+      // '/api': {
+      //   target: 'http://192.168.1.248:9888',
+      //   // target: 'http://192.168.1.4:8999',
+      //   pathRewrite: {
+      //     '^/api': '/api'
+      //   }
+      // }
+    // }
+  },
+  // pluginOptions: {
+  //   // 第三方插件配置
+  //   // ...
+  // }
 };
